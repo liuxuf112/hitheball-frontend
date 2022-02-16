11334840f2046a58ecc036e0c6cac8775b54ac97
Tanner <fossta@oregonstate.edu>
2022-02-16 09:08:20 -0800

    updated banner and text

--- lib/utils/helpers/design_constants.dart
+++ lib/utils/helpers/design_constants.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 //constant colors for things
 class AppConstants {
-  static const bannerColor = Color(0xFFCA4A4A);
+  static const bannerColor = Color(0xFFDFFF4F);
   static const bgColor = Color(0xFFB8B8B8);
   static const buttonColor = Color(0xFFFFFFFF);
   static const buttonSelectedBackgroundColor = Color(0xFFFFFFFF);
@@ -12,7 +12,8 @@ class AppConstants {
       Color(0xFFA2A2A2); //ARGB for some reason
   static const redTeamMapArea = Color(0x7FFF2012);
   static const blueTeamMapArea = Color(0x7F007CFF);
-  static const topOfAppBarText = Text("ECTF", textAlign: TextAlign.center);
+  static const topOfAppBarText = Text("Enhanced Tennis",
+      textAlign: TextAlign.center, style: TextStyle(color: Colors.black));
   static const titleTextStyle =
       TextStyle(fontSize: 30, overflow: TextOverflow.fade);
   static const bodyTextStyle = TextStyle(fontSize: 20);
